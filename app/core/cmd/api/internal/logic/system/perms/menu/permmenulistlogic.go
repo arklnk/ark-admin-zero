410a8db485bba75addcbc4d98375a2ef01c75267
Trevor-Lan <trevorlan@163.com>
2022-08-15 21:00:31 +0800

    refactor: use copier to copy data

--- app/core/cmd/api/internal/logic/system/perms/menu/permmenulistlogic.go
+++ app/core/cmd/api/internal/logic/system/perms/menu/permmenulistlogic.go
@@ -7,6 +7,7 @@ import (
 	"ark-zero-admin/app/core/cmd/api/internal/types"
 	"ark-zero-admin/common/errorx"
 
+	"github.com/jinzhu/copier"
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
@@ -32,18 +33,10 @@ func (l *PermMenuListLogic) PermMenuList() (resp *types.PermMenuListResp, err er
 	var menu types.PermMenu
 	var PermMenuList []types.PermMenu
 	for _, v := range permMenus {
-		menu.Id = v.Id
-		menu.ParentId = v.ParentId
-		menu.Name = v.Name
-		menu.Router = v.Router
-		menu.Perms = v.Perms
-		menu.Type = v.Type
-		menu.Icon = v.Icon
-		menu.OrderNum = v.OrderNum
-		menu.ViewPath = v.ViewPath
-		menu.IsShow = v.IsShow
-		menu.ActiveRouter = v.ActiveRouter
-
+		err := copier.Copy(&menu, &v)
+		if err != nil {
+			return nil, errorx.NewDefaultError(errorx.PasswordErrorCode)
+		}
 		PermMenuList = append(PermMenuList, menu)
 	}
 	return &types.PermMenuListResp{PermMenuList: PermMenuList}, nil
