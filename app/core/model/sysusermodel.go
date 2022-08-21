b93d9e708f998c8d4b36a6b41a66a970826c38d0
Trevor-Lan <trevorlan@163.com>
2022-08-21 12:16:38 +0800

    fix: user total is 1 larger than displayed

--- app/core/model/sysusermodel.go
+++ app/core/model/sysusermodel.go
@@ -87,7 +87,7 @@ func (m *customSysUserModel) FindCountByCondition(ctx context.Context, condition
 }
 
 func (m *customSysUserModel) FindCountByDeptIds(ctx context.Context, deptIds string) (int64, error) {
-	query := fmt.Sprintf("SELECT COUNT(id) FROM %s WHERE dept_id IN(%s)", m.table, deptIds)
+	query := fmt.Sprintf("SELECT COUNT(id) FROM %s WHERE id!=%d AND dept_id IN(%s)", m.table, globalkey.SysSuperAdminUserId, deptIds)
 	var resp int64
 	err := m.QueryRowNoCacheCtx(ctx, &resp, query)
 	switch err {
