3d549f2bd56023f68c9a1edd15821ddc1b328602
Trevor-Lan <trevorlan@163.com>
2022-08-22 16:02:15 +0800

    feat: UpdateParamConfig is complete

--- app/core/cmd/api/internal/logic/param/config/updateparamconfiglogic.go
+++ app/core/cmd/api/internal/logic/param/config/updateparamconfiglogic.go
@@ -5,7 +5,9 @@ import (
 
 	"ark-admin-zero/app/core/cmd/api/internal/svc"
 	"ark-admin-zero/app/core/cmd/api/internal/types"
+	"ark-admin-zero/common/errorx"
 
+	"github.com/jinzhu/copier"
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
@@ -24,7 +26,20 @@ func NewUpdateParamConfigLogic(ctx context.Context, svcCtx *svc.ServiceContext) 
 }
 
 func (l *UpdateParamConfigLogic) UpdateParamConfig(req *types.UpdateParamConfigReq) error {
-	// todo: add your logic here and delete this line
+	paramConfig, err := l.svcCtx.SysConfigModel.FindOne(l.ctx, req.Id)
+	if err != nil {
+		return errorx.NewDefaultError(errorx.ServerErrorCode)
+	}
+
+	err = copier.Copy(paramConfig, req)
+	if err != nil {
+		return errorx.NewDefaultError(errorx.ServerErrorCode)
+	}
+
+	err = l.svcCtx.SysConfigModel.Update(l.ctx, paramConfig)
+	if err != nil {
+		return errorx.NewDefaultError(errorx.ServerErrorCode)
+	}
 
 	return nil
 }
