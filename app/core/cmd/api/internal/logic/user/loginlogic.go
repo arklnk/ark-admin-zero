079faf54441274b36d9279c725b528adc7376b7d
Trevor-Lan <trevorlan@163.com>
2022-08-24 14:35:24 +0800

    fix: superadmin login

--- app/core/cmd/api/internal/logic/user/loginlogic.go
+++ app/core/cmd/api/internal/logic/user/loginlogic.go
@@ -50,9 +50,11 @@ func (l *LoginLogic) Login(req *types.LoginReq, r *http.Request) (resp *types.Lo
 		return nil, errorx.NewDefaultError(errorx.AccountDisableErrorCode)
 	}
 
-	dept, err := l.svcCtx.SysDeptModel.FindOne(l.ctx, sysUser.DeptId)
-	if dept.Status == 0 {
-		return nil, errorx.NewDefaultError(errorx.AccountDisableErrorCode)
+	if sysUser.Id != config.SysSuperAdminUserId {
+		dept, _ := l.svcCtx.SysDeptModel.FindOne(l.ctx, sysUser.DeptId)
+		if dept.Status == 0 {
+			return nil, errorx.NewDefaultError(errorx.AccountDisableErrorCode)
+		}
 	}
 
 	token, _ := l.getJwtToken(sysUser.Id)
