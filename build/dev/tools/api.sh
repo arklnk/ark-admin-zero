8a26d96d3dedf045f4aced394042aa55471d5da2
Trevor-Lan <trevorlan@163.com>
2022-10-04 04:43:40 +0800

    chore: adjust the directory structure

---
+++ build/dev/tools/api.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+dir=$1
+api=$2
+
+# shellcheck disable=SC2086
+goctl api go -api "${dir}"/${api} -dir ${dir} -home /server/build/dev/goctl
--- dev/tools/api.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-dir=$1
-api=$2
-
-# shellcheck disable=SC2086
-goctl api go -api "${dir}"/${api} -dir ${dir} -home /server/dev/goctl
